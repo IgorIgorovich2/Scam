7df1dbb6036d5bb4736c583389c303ad5895c851
IgorIgorovich2 <110422753+IgorIgorovich2@users.noreply.github.com>
2024-09-25 21:48:59 +0300

    add imports and install packages

---
+++ Scam/Scam/src/main.py
@@ -0,0 +1,6 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '8028581214:AAFrZz_Jqr_pwQQ_nXZXIVBjZPJ-Mx7hJn4'
+BOT_USERNAME: Final = '@ISK_SHEPOT_BOT'
